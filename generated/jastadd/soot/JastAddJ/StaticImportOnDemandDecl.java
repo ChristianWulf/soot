b255e4e5a93ba2a2bb4e1d9867e1ededf60083cf
Christian Wulf <chw@informatik.uni-kiel.de>
2017-07-31 17:20:35 +0200

    fixed single static import decl

--- generated/jastadd/soot/JastAddJ/StaticImportOnDemandDecl.java
+++ generated/jastadd/soot/JastAddJ/StaticImportOnDemandDecl.java
@@ -1,23 +1,6 @@
 /* This file was generated with JastAdd2 (http://jastadd.org) version R20130212 (r1031) */
 package soot.JastAddJ;
 
-import java.util.HashSet;
-import java.io.File;
-import java.util.*;
-import beaver.*;
-import java.util.ArrayList;
-import java.util.zip.*;
-import java.io.*;
-import java.io.FileNotFoundException;
-import java.util.Collection;
-import soot.*;
-import soot.util.*;
-import soot.jimple.*;
-import soot.coffi.ClassFile;
-import soot.coffi.method_info;
-import soot.coffi.CONSTANT_Utf8_info;
-import soot.tagkit.SourceFileTag;
-import soot.coffi.CoffiMethodSource;
 /**
  * 7.5.4 A static-import-on-demand declaration allows all accessible (\ufffd\ufffd6.6) static
  * members declared in the type named by a canonical name to be imported as
@@ -30,19 +13,22 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
   /**
    * @apilevel low-level
    */
-  public void flushCache() {
+  @Override
+public void flushCache() {
     super.flushCache();
   }
   /**
    * @apilevel internal
    */
-  public void flushCollectionCache() {
+  @Override
+public void flushCollectionCache() {
     super.flushCollectionCache();
   }
   /**
    * @apilevel internal
    */
-  @SuppressWarnings({"unchecked", "cast"})
+  @Override
+@SuppressWarnings({"unchecked", "cast"})
   public StaticImportOnDemandDecl clone() throws CloneNotSupportedException {
     StaticImportOnDemandDecl node = (StaticImportOnDemandDecl)super.clone();
     node.in$Circle(false);
@@ -52,13 +38,15 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
   /**
    * @apilevel internal
    */
-  @SuppressWarnings({"unchecked", "cast"})
+  @Override
+@SuppressWarnings({"unchecked", "cast"})
   public StaticImportOnDemandDecl copy() {
     try {
-      StaticImportOnDemandDecl node = (StaticImportOnDemandDecl) clone();
+      StaticImportOnDemandDecl node = clone();
       node.parent = null;
-      if(children != null)
-        node.children = (ASTNode[]) children.clone();
+      if(children != null) {
+		node.children = children.clone();
+	}
       return node;
     } catch (CloneNotSupportedException e) {
       throw new Error("Error: clone not supported for " +
@@ -71,12 +59,13 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
    * @return dangling copy of the subtree at this node
    * @apilevel low-level
    */
-  @SuppressWarnings({"unchecked", "cast"})
+  @Override
+@SuppressWarnings({"unchecked", "cast"})
   public StaticImportOnDemandDecl fullCopy() {
-    StaticImportOnDemandDecl tree = (StaticImportOnDemandDecl) copy();
+    StaticImportOnDemandDecl tree = copy();
     if (children != null) {
       for (int i = 0; i < children.length; ++i) {
-        ASTNode child = (ASTNode) children[i];
+        ASTNode child = children[i];
         if(child != null) {
           child = child.fullCopy();
           tree.setChild(child, i);
@@ -86,18 +75,19 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
     return tree;
   }
   /**
-   * @ast method 
+   * @ast method
    * @aspect StaticImports
    * @declaredat /Users/eric/Documents/workspaces/clara-soot/JastAddJ/Java1.5Frontend/StaticImports.jrag:213
    */
-  public void toString(StringBuffer s) {
+  @Override
+public void toString(final StringBuffer s) {
     s.append("import static ");
     getAccess().toString(s);
     s.append(".*;\n");
   }
   /**
-   * @ast method 
-   * 
+   * @ast method
+   *
    */
   public StaticImportOnDemandDecl() {
     super();
@@ -109,53 +99,58 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
    * Initializes List and Opt nta children.
    * @apilevel internal
    * @ast method
-   * @ast method 
-   * 
+   * @ast method
+   *
    */
-  public void init$Children() {
+  @Override
+public void init$Children() {
     children = new ASTNode[1];
   }
   /**
-   * @ast method 
-   * 
+   * @ast method
+   *
    */
-  public StaticImportOnDemandDecl(Access p0) {
+  public StaticImportOnDemandDecl(final Access p0) {
     setChild(p0, 0);
   }
   /**
    * @apilevel low-level
-   * @ast method 
-   * 
+   * @ast method
+   *
    */
-  protected int numChildren() {
+  @Override
+protected int numChildren() {
     return 1;
   }
   /**
    * @apilevel internal
-   * @ast method 
-   * 
+   * @ast method
+   *
    */
-  public boolean mayHaveRewrite() {
+  @Override
+public boolean mayHaveRewrite() {
     return false;
   }
   /**
    * Replaces the Access child.
    * @param node The new node to replace the Access child.
    * @apilevel high-level
-   * @ast method 
-   * 
+   * @ast method
+   *
    */
-  public void setAccess(Access node) {
+  @Override
+public void setAccess(final Access node) {
     setChild(node, 0);
   }
   /**
    * Retrieves the Access child.
    * @return The current node used as the Access child.
    * @apilevel high-level
-   * @ast method 
-   * 
+   * @ast method
+   *
    */
-  public Access getAccess() {
+  @Override
+public Access getAccess() {
     return (Access)getChild(0);
   }
   /**
@@ -163,10 +158,11 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
    * <p><em>This method does not invoke AST transformations.</em></p>
    * @return The current node used as the Access child.
    * @apilevel low-level
-   * @ast method 
-   * 
+   * @ast method
+   *
    */
-  public Access getAccessNoTransform() {
+  @Override
+public Access getAccessNoTransform() {
     return (Access)getChildNoTransform(0);
   }
   /**
@@ -174,7 +170,8 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
    * @aspect StaticImports
    * @declaredat /Users/eric/Documents/workspaces/clara-soot/JastAddJ/Java1.5Frontend/StaticImports.jrag:53
    */
-  public TypeDecl type() {
+  @Override
+public TypeDecl type() {
     ASTNode$State state = state();
     try {  return getAccess().type();  }
     finally {
@@ -185,7 +182,8 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
    * @aspect TypeScopePropagation
    * @declaredat /Users/eric/Documents/workspaces/clara-soot/JastAddJ/Java1.4Frontend/LookupType.jrag:351
    */
-  public boolean isOnDemand() {
+  @Override
+public boolean isOnDemand() {
     ASTNode$State state = state();
     try {  return true;  }
     finally {
@@ -195,7 +193,8 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
    * @declaredat /Users/eric/Documents/workspaces/clara-soot/JastAddJ/Java1.5Frontend/StaticImports.jrag:204
    * @apilevel internal
    */
-  public NameType Define_NameType_nameType(ASTNode caller, ASTNode child) {
+  @Override
+public NameType Define_NameType_nameType(final ASTNode caller, final ASTNode child) {
     if(caller == getAccessNoTransform()) {
       return NameType.TYPE_NAME;
     }
@@ -205,7 +204,9 @@ public class StaticImportOnDemandDecl extends StaticImportDecl implements Clonea
   /**
    * @apilevel internal
    */
-  public ASTNode rewriteTo() {
+  @Override
+public ASTNode rewriteTo() {
     return super.rewriteTo();
   }
+
 }
